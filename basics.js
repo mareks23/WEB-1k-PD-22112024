41cdd41b386d0c6842c6dfbf0ccd08db72fcd07f
unknown <you@example.com>
2024-11-22 10:22:03 +0200

    first commit

---
+++ basics.js
@@ -0,0 +1,21 @@
+let personAge = 18;
+
+
+  if (i = personAge, i<=18){
+    console.log("nepilngadīgs");
+
+    if (i = personAge, i>=18){
+        console.log("pilngadīgs");
+      }
+  }
+
+
+  const cilveki = ["Jānis", "Anna", "Zaiga"];
+
+
+
+
+
+  
+  
+  
